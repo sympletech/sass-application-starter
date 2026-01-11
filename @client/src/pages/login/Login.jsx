1716ebd6766aab6c6c8de55956fa1427aa53a84b
Daniel Lewis <dlewis@sympletech.com>
2026-01-10 23:06:16 -0800

    added login with X Icon

--- @client/src/pages/login/Login.jsx
+++ @client/src/pages/login/Login.jsx
@@ -7,7 +7,7 @@ import './Login.css';
 
 // Google Logo SVG Component
 const GoogleIcon = () => (
-    <svg width="18" height="18" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg" style={{ marginRight: '8px' }}>
+    <svg width="18" height="18" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
         <g fill="none" fillRule="evenodd">
             <path d="M17.64 9.205c0-.639-.057-1.252-.164-1.841H9v3.481h4.844a4.14 4.14 0 0 1-1.796 2.716v2.259h2.908c1.702-1.567 2.684-3.875 2.684-6.615z" fill="#4285F4" />
             <path d="M9 18c2.43 0 4.467-.806 5.956-2.18l-2.908-2.259c-.806.54-1.837.86-3.048.86-2.344 0-4.328-1.584-5.036-3.711H.957v2.332A8.997 8.997 0 0 0 9 18z" fill="#34A853" />
@@ -19,8 +19,15 @@ const GoogleIcon = () => (
 
 // Facebook Logo SVG Component
 const FacebookIcon = () => (
-    <svg width="18" height="18" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" style={{ marginRight: '8px' }}>
+    <svg width="18" height="18" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" fill="#1877F2" />
+    </svg>
+);
+
+// X (Twitter) Logo SVG Component
+const XIcon = () => (
+    <svg width="18" height="18" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" fill="currentColor" />
     </svg>
 );
 
@@ -38,6 +45,10 @@ function Login() {
 
     const handleFacebookLogin = () => {
         window.location.href = `${apiBaseUrl}/auth/facebook?returnURI=${encodeURIComponent(window.location.href)}`;
+    };
+
+    const handleXLogin = () => {
+        window.location.href = `${apiBaseUrl}/auth/twitter?returnURI=${encodeURIComponent(window.location.href)}`;
     };
 
     return (
@@ -110,6 +121,17 @@ function Login() {
                 >
                     <FacebookIcon />
                     Login with Facebook
+                </Button>
+
+                <Button
+                    className="oauth-login-button"
+                    size="large"
+                    block
+                    onClick={handleXLogin}
+                    style={{ marginTop: '12px' }}
+                >
+                    <XIcon />
+                    Login with X
                 </Button>
 
                 <div className="login-footer">
