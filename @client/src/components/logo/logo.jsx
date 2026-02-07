6748980545e29eb3c3033ec193e7ba0dc7b670a7
Daniel Lewis <dlewis@sympletech.com>
2026-02-06 23:40:23 -0800

    added logo

--- @client/src/components/logo/logo.jsx
+++ @client/src/components/logo/logo.jsx
@@ -1,31 +1,22 @@
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
+import sympletechLogo from '@client/assets/sympletech-logo.png';
+
 const Logo = ({ 
     href, 
-    className = '', 
-    showText = true, 
-    isDarkMode = false 
+    className = ''
 }) => {
     return (
         <Link to={href} className={`flex items-center h-full no-underline ${className}`}>
-            <div className="w-10 h-10 bg-gradient-alt rounded-lg flex items-center justify-center text-text-inverse font-bold text-xl mr-3 shadow-soft">
-                S
-            </div>
-            {showText && (
-                <span className={`text-lg font-semibold ${isDarkMode ? 'text-text-inverse' : 'text-text-strong'}`}>
-                    Sympletech
-                </span>
-            )}
+            <img src={sympletechLogo} alt="Sympletech Application Starter" className="w-150 h-10 mr-3" />
         </Link>
     );
 };
 
 Logo.propTypes = {
     href: PropTypes.string.isRequired,
-    className: PropTypes.string,
-    showText: PropTypes.bool,
-    isDarkMode: PropTypes.bool,
+    className: PropTypes.string
 };
 
 export default Logo;
